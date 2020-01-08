4a4713ccbc4c5f5b4d64da21a3f38d5d207a338a
Gabriel Alves <g04lves@gmail.com>
2020-01-08 13:08:52 -0300

    Refactor names of ticks to match message structures

--- src/module_b.rs
+++ src/module_b.rs
@@ -8,13 +8,13 @@ pub fn new(a_sender: crossbeam_channel::Sender<Box<dyn Message>>) -> thread::Joi
 }
 
 fn run(sender: crossbeam_channel::Sender<Box<dyn Message>>) {
-    let periodic_ticks = crossbeam_channel::tick(std::time::Duration::from_secs(5));
-    let periodic_ticks_2 = crossbeam_channel::tick(std::time::Duration::from_secs(2));
+    let first_message_tick = crossbeam_channel::tick(std::time::Duration::from_secs(5));
+    let second_message_tick = crossbeam_channel::tick(std::time::Duration::from_secs(2));
 
     loop {
         crossbeam_channel::select! {
-            recv(periodic_ticks) -> _ => { sender.send(Box::new(FirstM {})).unwrap(); }
-            recv(periodic_ticks_2) -> _ => { sender.send(Box::new(SecondM {})).unwrap(); }
+            recv(first_message_tick) -> _ => { sender.send(Box::new(FirstM {})).unwrap(); }
+            recv(second_message_tick) -> _ => { sender.send(Box::new(SecondM {})).unwrap(); }
             default(std::time::Duration::from_millis(100)) => (),
         };
     }
